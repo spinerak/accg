e08f47dadf79a2e7e8b4494db82a188099fba6e9
b.v.arnhem@student.tue.nl <b.v.arnhem@student.tue.nl@3bde6eee-7e5d-11dd-9205-735403a6da14>
2008-11-24 12:48:43 +0000

    MC fix

--- source/CSGBuilder/src/csgbuilder/CSGBuilderView.java
+++ source/CSGBuilder/src/csgbuilder/CSGBuilderView.java
@@ -43,7 +43,7 @@ public class CSGBuilderView extends FrameView {
 		
 	// Create a CSG Tree
         lvTree = new CSGTree(new CSGEllipsoid(new double[]{0.0,0.0,0.0}, new double[]{1.0,1.0,1.0}));
-        lvTree.difference(new CSGEllipsoid(new double[]{0.8,0.8,0.8}, new double[]{1.0,1.0,1.0}));
+        lvTree.intersect(new CSGEllipsoid(new double[]{0.8,0.8,0.8}, new double[]{1.0,1.0,1.0}));
 			
 	// Get the mesh for this tree
 	CSGTreePolygoniser lvPolygoniser = new CSGTreePolygoniser(lvAOperandViewer, lvTree);
